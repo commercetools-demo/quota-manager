5cbd7053b777363041c5ffced26650c1a694935d
Philipp Hofmann <philipp.hofmann@commercetools.com>
2024-07-01 17:21:29 +0200

    as connect?

---
+++ quota-manager-service/src/controllers/cart.controller.ts
@@ -0,0 +1,25 @@
+import CustomError from '../errors/custom.error';
+import { Resource } from '../interfaces/resource.interface';
+import { logger } from '../utils/logger.utils';
+
+/**
+ * Handle the cart controller according to the action
+ *
+ * @param {string} action The action that comes with the request. Could be `Create` or `Update`
+ * @param {Resource} resource The resource from the request body
+ * @returns {Promise<object>} The data from the method that handles the action
+ */
+export const cartController = async (action: string, resource: Resource) => {
+  switch (action) {
+    case 'Update':
+      logger.info('Cart update extension executed', resource);
+      return { statusCode: 200 };
+      break;
+
+    default:
+      throw new CustomError(
+        500,
+        `Internal Server Error - Resource not recognized. Allowed values are 'Create' or 'Update'.`
+      );
+  }
+};
